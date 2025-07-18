78e2797013b5e3ef50b2a4e28e668ef81efea13e
mohamweb <mohamed_mol@hotmail.com>
2025-07-18 23:38:08 +0300

    asd

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // index.js
 
-const express = require('express')
-const {render} = require('ejs')
+const express = require("express")
+const ejs = require("ejs")
 const app = express()
 const PORT = 4000 || process.env.port;
 
 
 app.get('/', (req, res) => {
   // res.status(200).json('Welcome, your app is working well');
-  res.render('Cruds.ejs');
+  res.render("Cruds.ejs");
 })
 
 
